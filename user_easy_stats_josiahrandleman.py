4dee2ef80873d116bd57ec94ddd5c42922b0e17b
Josiah Randleman <jrandl516@gmail.com>
2023-08-31 00:05:07 -0500

    updated the data in my file

--- user_easy_stats_josiahrandleman.py
+++ user_easy_stats_josiahrandleman.py
@@ -1,99 +1,134 @@
-# Import the statistics module for statistical calculations
-import statistics
+"""
+Author: Josiah Randleman
+Purpose: Illustrate the built-in statistics module.
 
-# Import the logger setup function from an instructor-provided module
+VS Code Menu / View / Command Palette / Python Interpreter
+Must be 3.10 or greater to get the correlation and linear regression.
+
+Uses only Python Standard Library modules.
+
+@ uses statistics module for descriptive stats
+@ uses sys module for checking Python version
+
+"""
+# ----------------- INSTRUCTOR GENERATED CODE -----------------
+
+# Use this handy logger to document your work automatically
+
+# import setup_logger function from instructor-generated module
 from util_logger import setup_logger
 
-# Set up the logger using the current file's name
+# setup the logger using the current file name (a built-in variable)
 logger, logname = setup_logger(__file__)
 
-# Define bivariate data for 'hours studied' and 'exam scores'
-study_hours_x = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-exam_scores_y = [65, 70, 75, 80, 85, 90, 92, 95, 98, 100]
-
-# Calculate descriptive statistics for 'exam_scores_y'
-# 1. Mean
-mean_scores = statistics.mean(exam_scores_y)
-# 2. Median
-median_scores = statistics.median(exam_scores_y)
-# 3. Mode (Be cautious, all values are unique in this dataset)
-mode_scores = statistics.mode(exam_scores_y)
-# 4. Variance
-variance_scores = statistics.variance(exam_scores_y)
-# 5. Standard Deviation
-std_deviation_scores = statistics.stdev(exam_scores_y)
-# 6. Minimum score
-min_score = min(exam_scores_y)
-# 7. Maximum score
-max_score = max(exam_scores_y)
-
-# Calculate descriptive statistics for 'study_hours_x'
-# 1. Mean
-mean_hours = statistics.mean(study_hours_x)
-# 2. Median
-median_hours = statistics.median(study_hours_x)
-# 3. Mode (Be cautious, all values are unique in this dataset)
-try:
-    mode_hours = statistics.mode(study_hours_x)
-except statistics.StatisticsError:
-    mode_hours = 'No unique mode'
-# 4. Variance
-variance_hours = statistics.variance(study_hours_x)
-# 5. Standard Deviation
-std_deviation_hours = statistics.stdev(study_hours_x)
-# 6. Minimum hours
-min_hours = min(study_hours_x)
-# 7. Maximum hours
-max_hours = max(study_hours_x)
+# ----------------- END INSTRUCTOR GENERATED CODE -----------------
 
 
+# Import from Python Standard Library
 
-# Manually calculate the range for study hours (difference between max and min)
-hours_range = max_hours - min_hours
-logger.info(f"Range: {hours_range:.2f}")
-logger.info("---------------------------------------")
+import statistics
+import sys
 
-# Log the calculated statistics
-logger.info("---------------------------------------")
-logger.info(f"Study Hours -> X Values {study_hours_x}")
-logger.info(f"Exam Scores -> Y Values {exam_scores_y}")
-logger.info("---------------------------------------")
-logger.info("Descriptive statistics for exam scores:")
-logger.info(f"Mean: {mean_scores:.2f}")
-logger.info(f"Median: {median_scores:.2f}")
-logger.info(f"Mode: {mode_scores:.2f}")
-logger.info("---------------------------------------")
-logger.info(f"Variance: {variance_scores:.2f}")
-logger.info(f"Standard Deviation: {std_deviation_scores:.2f}")
-logger.info(f"Smallest Score: {min_score:.2f}")
-logger.info(f"Largest Score: {max_score:.2f}")
+# Descriptive: Univariant Data..................................
 
-# Manually calculate the range (difference between max and min)
-score_range = max_score - min_score
-logger.info(f"Range: {score_range:.2f}")
-logger.info("---------------------------------------")
+# univariant data (one variable, many readings)
+# Yearly sales of a song over 10 years
+uni_data = [1200, 1500, 1300, 1700, 1450, 1600, 1550, 1800, 1750, 1900]
 
-# Log the calculated statistics for study hours
-logger.info("Descriptive statistics for study hours:")
-logger.info(f"Mean: {mean_hours:.2f}")
-logger.info(f"Median: {median_hours:.2f}")
-logger.info(f"Mode: {mode_hours}")
-logger.info(f"Variance: {variance_hours:.2f}")
-logger.info(f"Standard Deviation: {std_deviation_hours:.2f}")
-logger.info(f"Smallest Hours: {min_hours:.2f}")
-logger.info(f"Largest Hours: {max_hours:.2f}")
+logger.info("uni_data = " + str(uni_data))
 
-logger.info("---------------------------------------")
+# Descriptive: Averages and measures of central tendency
 
-# Calculate and log correlation coefficients
-xx_corr = statistics.correlation(study_hours_x, study_hours_x)
-xy_corr = statistics.correlation(study_hours_x, exam_scores_y)
-logger.info(f"Correlation between study hours and study hours = {xx_corr}")
-logger.info(f"Correlation between study hours and exam scores = {xy_corr}")
-logger.info("---------------------------------------")
+mean = statistics.mean(uni_data)
+median = statistics.median(uni_data)
+mode = statistics.mode(uni_data)
 
-# Calculate and log the slope and intercept using linear regression
-slope, intercept = statistics.linear_regression(study_hours_x, exam_scores_y)
-logger.info(f"Slope = {slope}")
-logger.info(f"Intercept = {intercept}")
-logger.info("---------------------------------------")
+# log use variable colon formatting to avoid unnecessary digits (e.g. .2f)
+
+logger.info(f"mean   = {mean:.2f}")
+logger.info(f"median = {median:.2f}")
+logger.info(f"mode   = {mode:.2f}")
+
+# Descriptive: Measures of spread
+
+var = statistics.variance(uni_data)
+stdev = statistics.stdev(uni_data)
+lowest = min(uni_data)
+highest = max(uni_data)
+
+# TODO: change to f-strings and display 2 decimal places (like we did above)
+logger.info(f"var    = {var:.2f}")
+logger.info(f"stdev  = {stdev:.2f}")
+logger.info(f"lowest = {lowest:.2f}")
+logger.info(f"highest= {highest:.2f}")
+logger.info(f"range= {highest - lowest:.2f}")
+
+
+# Descriptive: Univariant Time Series Data.........................
+
+# describe relationships
+# univariant time series data (one variable over time)
+# typically, x (or time) is independent and
+# y is dependent on x (e.g. temperature vs hour of day)
+xtimes = [1, 2, 3, 4, 5]  # Years on the market
+yvalues = [1000, 2000, 3000, 4000, 5000]  # Song sales
+
+
+# if the lists are not the same size,
+# log an error and quit the program
+if len(xtimes) != len(yvalues):
+    logger.error("ERROR: The related sets are not the same size.")
+    logger.error(f"      {len(xtimes)}!={len(yvalues)}")
+    quit()
+
+# check the Python version before using the correlation function
+logger.warn("Correlation requires Python version 3.10 or greater.")
+logger.warn(f"Your version is {sys.version_info.major}.{sys.version_info.minor}")
+
+# if the Python version is too old, we can quit now
+if sys.version_info.minor < 10:
+    logger.error("Please update Python to 3.10 or greater")
+    logger.error("or use View / Command Palette / Python: Select Interpreter")
+    logger.error("to get a newer one.")
+    quit()
+
+# If we're still here, use the new correlation function from the statistics module
+xx_corr = statistics.correlation(xtimes, xtimes)
+xy_corr = statistics.correlation(xtimes, yvalues)
+
+# log the information
+logger.info("Here's some time series data:")
+logger.info(f"xtimes:{xtimes}")
+logger.info(f"yvalues:{yvalues}")
+logger.info(f"correlation between xtimes and xtimes = {xx_corr:.2f}")
+logger.info(f"correlation between xtimes and yvalues = {xy_corr:.2f}")
+
+# Calculate slope and intercept of a line
+
+# Here's some bivariant data (two series of data)
+
+arrayX = [1, 2, 3, 4, 5]  # Years since song release
+arrayY = [1200, 2700, 4000, 5700, 7500]  # Cumulative song sales
+
+# Call linear_regression() function -
+# and get back 2 values: slope and intercept
+# describing the 'best fit' line through the data
+slope, intercept = statistics.linear_regression(arrayX, arrayY)
+
+# Choose an x value off in the future (future x)
+future_x = 200
+
+# Extend the line out into the unknown future
+# and read the value (of future y)
+future_y = round(slope * future_x + intercept)
+
+logger.info("Here's some bivariant data (2 variables, together):")
+logger.info(f"x:{arrayX}")
+logger.info(f"y:{arrayY}")
+logger.info("Calculate the slope and intercept of a best fit straight line:")
+logger.info(f"   slope = {slope:.2f}")
+logger.info(f"   intercept = { intercept:.2f}")
+logger.info("Let's use our best fit line to PREDICT a future value.")
+logger.info(f"   At future x = {future_x:d},")
+logger.info(f"   we predict the value of y will be { future_y:d}.")
+logger.info("How'd we do? Does this make sense given the data?")
